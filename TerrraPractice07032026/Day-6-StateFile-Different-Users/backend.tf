a180215d085b501874bb64df2c0c9e10f8035f49
PrasadNeGroup <prasad.boddu@negroup.com>
2026-03-14 00:02:50 +0530

    subnet-added

--- TerrraPractice07032026/Day-6-StateFile-Different-Users/backend.tf
+++ TerrraPractice07032026/Day-6-StateFile-Different-Users/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "dev-create-1"
+    bucket = "dev-create-2"
     key = "test/terraform.tfstate"
     region = "us-east-1"
-    use_lockfile = true
+    use_lockfile = false
   }
 }
--- TerrraPractice07032026/Day-6-StateFile-Different-Users/main.tf
+++ TerrraPractice07032026/Day-6-StateFile-Different-Users/main.tf
@@ -5,4 +5,11 @@ resource "aws_vpc" "project_vpc" {
     }
   
 }
-
+resource "aws_subnet" "dev_subnet" {
+    vpc_id = aws_vpc.project_vpc.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+      Name = "dev-subnet"
+    }
+  
+}
